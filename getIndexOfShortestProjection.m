76f56f66adf96f8e1620a640c9a260fa351d3926
caetius <dlcasabuena@gmail.com>
2017-08-21 01:23:18 +0100

    Add files via upload

--- getIndexOfShortestProjection.m
+++ getIndexOfShortestProjection.m
@@ -1,19 +1,19 @@
-% Find index of and projection of set of points to closest Euclidean subspace
-% Input: Set of points in R^d and 3D array of sets of (j+1)-d-dimensional points defining a j-subspace 
+% Find index of shortest projection of a point to an indexed set of Euclidean subspace
+% Input: A point in R^d and 3D array of sets of (j+1)-d-dimensional points defining a j-subspace 
 % Output: Indices of subspace closest to each point and the corresponding
 % projection.
 %Diego Lorenzo-Casabuena Gonzalez, 2017. Prof. Dan Feldman
 %%
 
-function [index, projection] = getIndexOfShortestProjection(point, subspaces)
+function [index, projection] = getIndexOfShortestProjection(point, subspaces,mode)
 
 num_sub = size(subspaces,3);
 current_score = intmax;
 
 for i = 1:num_sub
    
-    [proj, ~, ~] = projectPointsOntoSubspace(point, subspaces(:,:,i));
-    dist = abs(point-proj);
+    [proj, ~, ~,dist] = projectPointsOntoSubspace(point, subspaces(:,:,i));
+    dist = findDistanceScore(dist, mode, 1);
     if dist < current_score
        current_score = dist;
        index = i;
